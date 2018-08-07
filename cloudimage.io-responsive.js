6bda1e7717744f17cb28a348c2423401931ce937
dmitry <dmitry@scaleflex.cloud>
2018-08-07 13:10:48 +0300

    rectify code

--- cloudimage.io-responsive.js
+++ cloudimage.io-responsive.js
@@ -5,23 +5,24 @@
     config = config || {};
 
     jScaler.config = {
-      TOKEN:          config.TOKEN || '',
-      CONTAINER:      config.CONTAINER || '.cloudimg.io',
-      DEFAULT_WIDTH:  config.DEFAULT_WIDTH || '400',
+      IS_ULTRA_FAST: config.IS_ULTRA_FAST || false,
+      TOKEN: config.TOKEN || '',
+      CONTAINER: config.CONTAINER || '.cloudimg.io',
+      DEFAULT_WIDTH: config.DEFAULT_WIDTH || '400',
       DEFAULT_HEIGHT: config.DEFAULT_HEIGHT || '300',
-      DEFAULT_TYPE:   config.DEFAULT_TYPE || 'width',
+      DEFAULT_TYPE: config.DEFAULT_TYPE || 'width',
       DEFAULT_PARAMS: config.DEFAULT_PARAMS || 'none',
-      BASE_URL:       config.BASE_URL || '/',
-      PRESETS:        config.PRESETS ||
-                      {
-                        xs: 576,  // 0 - 576      PHONE
-                        sm: 768,  // 577 - 768    PHABLET
-                        md: 992,  // 769 - 992    TABLET
-                        lg: 1200, // 993 - 1200   SMALL_LAPTOP_SCREEN
-                        xl: 1920  // 1200 - 1920  USUALSCREEN
-                      },
-      ORDER:          config.ORDER || ['xl', 'lg', 'md', 'sm', 'xs'],
-      AUTO:           config.AUTO || [1920, 1200, 992, 768, 576]
+      BASE_URL: config.BASE_URL || '/',
+      PRESETS: config.PRESETS ||
+      {
+        xs: 576,  // 0 - 576      PHONE
+        sm: 768,  // 577 - 768    PHABLET
+        md: 992,  // 769 - 992    TABLET
+        lg: 1200, // 993 - 1200   SMALL_LAPTOP_SCREEN
+        xl: 1920  // 1200 - 1920  USUALSCREEN
+      },
+      ORDER: config.ORDER || ['xl', 'lg', 'md', 'sm', 'xs'],
+      AUTO: config.AUTO || [1920, 1200, 992, 768, 576]
     };
   };
 
@@ -37,15 +38,18 @@
   jScaler.process = function (config) {
     this.setConfig(config);
 
-    var imgs           = document.querySelectorAll('img[ci-src]'),
-        backgroundImgs = document.querySelectorAll('[ci-img-background]');
+    var imgs = filterImages(document.querySelectorAll('img[ci-src]'), 'ci-src'),
+      backgroundImgs = filterImages(document.querySelectorAll('[ci-img-background]'), 'ci-img-background');
 
     if (imgs.length > 0) {
       imgs = Array.prototype.slice.call(imgs);
       imgs.forEach(function (img) {
         img.addEventListener('error', onerrorImg, false);  //TODO: check if it works well
+
         if (!this.attr(img, 'src')) { //TODO: find better way
           this.processImage(img);
+        } else {
+          this.processImage(img, true)
         }
       }, this);
     }
@@ -60,17 +64,85 @@
     }
   };
 
-  jScaler.processImage = function (img) {
-    var sourceUrl    = 'ci-src',
-        imgType      = this.attr(img, 'ci-type') || this.config.DEFAULT_TYPE || '',
-        imgSize      = this.attr(img, 'ci-size') || this.getDefaultSize(imgType) || '',
-        imgParams    = this.attr(img, 'ci-params') || this.config.DEFAULT_PARAMS || '',
-        isLocalUrl   = this.isLocalURL(img, sourceUrl),
-        isResponsive = this.attr(img, 'ci-responsive') !== null, //TODO: need to check in all browsers
-        imgSrc       = this.getImgSrc(img, sourceUrl, isLocalUrl),
-        cloudimageUrl;
+  jScaler.getParentWidth = function (img) {
+    if (!(img && img.parentElement && img.parentElement.getBoundingClientRect) && !(img && img.width))
+      return jScaler.config.DEFAULT_WIDTH;
 
-    this.wrap(img);
+    var parentContainer = getParentContainerWithWidth(img);
+    var currentWidth = parseInt(parentContainer, 10);
+    var computedWidth = parseInt(window.getComputedStyle(img).width);
+
+    if ((computedWidth < currentWidth && computedWidth > 15) || !currentWidth) {
+      return getSizeLimit(computedWidth);
+    } else {
+      if (!currentWidth) return img.width || jScaler.config.DEFAULT_WIDTH;
+
+      return getSizeLimit(currentWidth);
+    }
+  }
+
+  jScaler.getElementSize = function (element, size) {
+    if (!element || !element.getBoundingClientRect)
+      return null;
+
+    var currentSize = parseInt(element.getBoundingClientRect()[size], 10);
+
+    return getSizeLimit(currentSize);
+  }
+
+  function getSizeLimit(currentSize) {
+    return currentSize <= 25 ? '25':
+      currentSize <= 50 ? '50':
+        currentSize <= 100 ? '100'
+          : currentSize <= 200 ? '200'
+          : currentSize <= 300 ? '300'
+            : currentSize <= 400 ? '400'
+              : currentSize <= 500 ? '500'
+                : currentSize <= 600 ? '600'
+                  : currentSize <= 700 ? '700'
+                    : currentSize <= 800 ? '800'
+                      : currentSize <= 900 ? '900'
+                        : currentSize <= 1000 ? '1000'
+                          : currentSize <= 1100 ? '1100'
+                            : currentSize <= 1200 ? '1200'
+                              : currentSize <= 1300 ? '1300'
+                                : currentSize <= 1400 ? '1400'
+                                  : currentSize <= 1500 ? '1500'
+                                    : currentSize <= 1600 ? '1600'
+                                      : currentSize <= 1700 ? '1700'
+                                        : currentSize <= 1800 ? '1800'
+                                          : currentSize <= 1900 ? '1900'
+                                            : currentSize <= 2400 ? '2400'
+                                              : currentSize <= 2800 ? '2800'
+                                                : '3600';
+  }
+
+  jScaler.checkOnMedia = function (size) {
+    try {
+      var object = eval('(' + size + ')');
+
+      return object && typeof object === "object";
+    }
+    catch (e) {
+      return false;
+    }
+  }
+
+  jScaler.processImage = function (img, isUpdate) {
+
+    var sourceUrl = 'ci-src',
+      imgType = this.attr(img, 'ci-type') || this.config.DEFAULT_TYPE || '',
+      imgSize = this.attr(img, 'ci-size') || this.getParentWidth(img) || '',
+      imgParams = this.attr(img, 'ci-params') || this.config.DEFAULT_PARAMS || '',
+      isLocalUrl = this.isLocalURL(img, sourceUrl),
+      isResponsive = this.checkOnMedia(imgSize),
+      imgSrc = this.getImgSrc(img, sourceUrl, isLocalUrl),
+      cloudimageUrl,
+      oldSize = img.getAttribute('data-old-ci-size');
+
+    if (oldSize && isUpdate && imgSize <= oldSize) return ;
+
+    img.setAttribute('data-old-ci-size', imgSize);
 
     if (isResponsive) {
       imgSize = eval('(' + imgSize + ')');
@@ -78,38 +150,72 @@
     } else {
       cloudimageUrl = this.generateUrl(imgType, imgSize, imgParams, imgSrc);
     }
-    img.setAttribute('src', cloudimageUrl);
-    this.addSources(img, imgType, imgSize, imgParams, imgSrc, isResponsive);
+    if (!isUpdate) {
+      this.wrap(img);
+
+      img.setAttribute('src', cloudimageUrl);
+      this.addSources(img, imgType, imgSize, imgParams, imgSrc, isResponsive);
+    } else {
+      var wrapper = img.parentNode.parentNode;
+      var sources = wrapper.querySelectorAll('source');
+
+      sources.forEach(function (item) { item.remove(); })
+
+      img.setAttribute('src', cloudimageUrl);
+      this.addSources(img, imgType, imgSize, imgParams, imgSrc, isResponsive);
+    }
   };
 
   jScaler.processBackgroundImage = function (elem) {
-    var sourceUrl    = 'ci-img-background',
-        isResponsive = this.attr(elem, 'ci-responsive') !== null, //TODO: need to check in all browsers
-        imgType      = this.attr(elem, 'ci-type') || this.config.DEFAULT_TYPE || '',
-        imgSize      = this.getBackgroundImgSize(elem, imgType, isResponsive),
-        imgParams    = this.attr(elem, 'ci-params') || this.config.DEFAULT_PARAMS || '',
-        imgSrc       = this.getBackgroundImgUrl(elem, sourceUrl);
+    var sourceUrl = 'ci-img-background',
+      imgType = this.attr(elem, 'ci-type') || this.config.DEFAULT_TYPE || '',
+      imgSize = this.attr(elem, 'ci-size') || this.setSizeFromElemProperty(elem, imgType) || this.getDefaultSize(imgType),
+      isResponsive = this.checkOnMedia(imgSize),
+      imgParams = this.attr(elem, 'ci-params') || this.config.DEFAULT_PARAMS || '',
+      imgSrc = this.getBackgroundImgUrl(elem, sourceUrl),
+      oldIndex = elem.getAttribute('ci-img-index'),
+      nextIndex = null;
 
-    elem.setAttribute('ci-img-index', this.backgroundImgIndex.toString());
-    this.backgroundImgIndex++;
 
-    this.addCss(elem, imgType, imgSize, imgParams, imgSrc, isResponsive);
+    if (!(oldIndex >= 0 && oldIndex !== null)) {
+      nextIndex = this.backgroundImgIndex.toString();
+      elem.setAttribute('ci-img-index', nextIndex);
+      this.backgroundImgIndex++;
+    } else {
+      nextIndex = oldIndex;
+    }
+
+    this.addCss(elem, imgType, imgSize, imgParams, imgSrc, isResponsive, nextIndex);
   };
 
   jScaler.generateUrl = function (imgType, imgSize, imgParams, imgSrc) {
-    var cloudUrl = '//' + this.config.TOKEN + this.config.CONTAINER + '/';
+    var ultraFast = this.config.IS_ULTRA_FAST ? 'https://scaleflex.ultrafast.io/' : 'https://';
+    var cloudUrl = ultraFast + this.config.TOKEN + this.config.CONTAINER + '/';
 
     return cloudUrl + imgType + '/' + imgSize + '/' + imgParams + '/' + imgSrc;
   };
 
   jScaler.addSources = function (img, imgType, imgSize, imgParams, imgSrc, isResponsive) {
     if (isResponsive) {
-      for (var size in imgSize) {
-        if (imgSize.hasOwnProperty(size)) {
-          var srcSet     = this.generateSrcset(imgType, imgSize[size], imgParams, imgSrc),
-              mediaQuery = '(max-width:' + this.config.PRESETS[size] + 'px)';
-          this.before(img, '<source media="' + mediaQuery + '" srcset="' + srcSet + '">');
-        }
+      var orderFiltered = [];
+
+
+      for (i = 0; i < this.config.ORDER.length; i++) {
+        var size = imgSize[this.config.ORDER[i]];
+
+        if (size)
+          orderFiltered.unshift(this.config.ORDER[i]);
+      }
+
+      for (var i = 0; i < orderFiltered.length; i++) {
+        var isLast = !(i < orderFiltered.length - 1);
+        var nextSizeType = isLast ? orderFiltered[i - 1] : orderFiltered[i];
+        var nextSize = imgSize[orderFiltered[i]];
+
+        var srcSet = this.generateSrcset(imgType, nextSize, imgParams, imgSrc),
+          mediaQuery = '(' + (isLast ? 'min' : 'max') +'-width: ' + (this.config.PRESETS[nextSizeType] + (isLast ? 1 : 0)) + 'px)';
+
+        this.before(img, '<source media="' + mediaQuery + '" srcset="' + srcSet + '">');
       }
     } else {
       this.before(img, '<source srcset="' + this.generateSrcset(imgType, imgSize, imgParams, imgSrc) + '">');
@@ -127,7 +233,7 @@
 
   jScaler.getBackgroundImgUrl = function (elem, sourceUrl) {
     var imgSrc = this.attr(elem, sourceUrl) || '',
-        isLocalUrl;
+      isLocalUrl;
     if (!imgSrc) {
       this.setUrlFromElemProperty(elem, sourceUrl);
     }
@@ -141,27 +247,32 @@
     if (imgHeight) {
       imgSize += 'x' + Math.trunc(imgHeight * factor);
     }
-    return this.generateUrl(imgType, imgSize, imgParams, imgSrc);
+    return this.generateUrl(imgType, imgSize, imgParams, imgSrc)
+      .replace('http://scaleflex.ultrafast.io/', '')
+      .replace('https://scaleflex.ultrafast.io/', '')
+      .replace('//scaleflex.ultrafast.io/', '')
+      .replace('///', '/');
   };
 
   jScaler.generateSrcset = function (imgType, imgSize, imgParams, imgSrc) {
-    var imgWidth  = imgSize.toString().split('x')[0],
-        imgHeight = imgSize.toString().split('x')[1];
+    var imgWidth = imgSize.toString().split('x')[0],
+      imgHeight = imgSize.toString().split('x')[1];
     return this.generateImgSrc(imgType, imgParams, imgSrc, imgWidth, imgHeight, 1) + ' 1x, ' +
       this.generateImgSrc(imgType, imgParams, imgSrc, imgWidth, imgHeight, 1.5) + ' 1.5x, ' +
       this.generateImgSrc(imgType, imgParams, imgSrc, imgWidth, imgHeight, 2) + ' 2x, ' +
       this.generateImgSrc(imgType, imgParams, imgSrc, imgWidth, imgHeight, 3) + ' 3x';
   };
 
-  jScaler.generateMediaQueries = function (elem, imgType, imgSize, imgParams, imgSrc, isResponsive, mediaQuery) {
+  jScaler.generateMediaQueries = function (elem, imgType, imgSize, imgParams, imgSrc, isResponsive, mediaQuery, isFirst, prevMediaQuery) {
     var selector_suffix = this.attr(elem, 'ci-selector-suffix') || '',
-        selector            = '[ci-img-index="' + this.attr(elem, 'ci-img-index') + '"]' + selector_suffix,
-        importantStr     = !!this.attr(elem, 'ci-important') ? ' !important' : '',
-        imgWidth            = imgSize.toString().split('x')[0],
-        imgHeight           = imgSize.toString().split('x')[1],
-        mediaQueryComplex   = isResponsive ? ' (max-width: ' + mediaQuery + 'px) and ' : '',
-        mediaQuerySingle    = isResponsive ? '@media (max-width: ' + mediaQuery + 'px) {' : '',
-        singleCloseBrackets = isResponsive ? '} ' : '';
+      selector = '[ci-img-index="' + this.attr(elem, 'ci-img-index') + '"]' + selector_suffix,
+      importantStr = !!this.attr(elem, 'ci-important') ? ' !important' : '',
+      imgWidth = imgSize.toString().split('x')[0],
+      imgHeight = imgSize.toString().split('x')[1],
+      query = isFirst ? prevMediaQuery : mediaQuery,
+      mediaQueryComplex = isResponsive ? ' (' + (!isFirst ? 'max' : 'min') + '-width: ' + query + 'px) and ' : '',
+      mediaQuerySingle = isResponsive ? '@media (' + (!isFirst ? 'max' : 'min') + '-width: ' + query + 'px) {' : '',
+      singleCloseBrackets = isResponsive ? '} ' : '';
 
     return mediaQuerySingle + selector + ' { background-image: url(' +
       this.generateImgSrc(imgType, imgParams, imgSrc, imgWidth, imgHeight, 1) + ')' + importantStr + '; } ' + singleCloseBrackets +
@@ -201,27 +312,19 @@
   };
 
   jScaler.setSizeFromElemProperty = function (elem, imgType) {
-    //TODO: rectify
     var width  = parseInt(window.getComputedStyle(elem).width),
-        height = parseInt(window.getComputedStyle(elem).height);
-    if (width && imgType === 'width') {
-      return width;
-    } else if (height && imgType === 'width') {
-      return height;
+      height = parseInt(window.getComputedStyle(elem).height);
+
+    if (imgType === 'width') {
+      return this.getElementSize(elem, 'width');
+    } else if (imgType === 'width') {
+      return this.getElementSize(elem, 'height');
     } else if (width && height) {
       return width + 'x' + height;
     }
   };
 
-  jScaler.getBackgroundImgSize = function (elem, imgType, isResponsive) {
-    var imgSize = this.attr(elem, 'ci-size') || '';
-    if (!imgSize && !isResponsive) {
-      imgSize = this.setSizeFromElemProperty(elem, imgType) || this.getDefaultSize(imgType);
-    }
-    return imgSize || '';
-  };
-
-  jScaler.addCss = function (elem, imgType, imgSize, imgParams, imgSrc, isResponsive) {
+  jScaler.addCss = function (elem, imgType, imgSize, imgParams, imgSrc, isResponsive, nextIndex) {
     var i, size, mediaQuery;
     if (isResponsive) {
       var cssQueries = '';
@@ -232,36 +335,54 @@
           imgSize = this.config.AUTO[i];
           cssQueries += this.generateMediaQueries(elem, 'width', imgSize, imgParams, imgSrc, isResponsive, imgSize);
         }
-      } else if (isImgSizeIsObject(imgSize)) {
+      } else if (this.checkOnMedia(imgSize)) {
         imgSize = eval('(' + imgSize + ')');
+        var orderFiltered = [];
+
         for (i = 0; i < this.config.ORDER.length; i++) {
           size = imgSize[this.config.ORDER[i]];
+
+          if (size)
+            orderFiltered.push(this.config.ORDER[i]);
+        }
+
+        for (i = 0; i < orderFiltered.length; i++) {
+          var isFirst = i === 0;
+          var prevSize = isFirst && orderFiltered[i + 1];
+          var prevMediaQuery = isFirst && this.config.PRESETS[prevSize]
+          size = imgSize[orderFiltered[i]];
           mediaQuery = this.config.PRESETS[this.config.ORDER[i]];
-          if (size) {
-            cssQueries += this.generateMediaQueries(
-              elem, imgType, size, imgParams, imgSrc, isResponsive, mediaQuery
-            );
-          }
+
+          cssQueries += this.generateMediaQueries(
+            elem, imgType, size, imgParams, imgSrc, isResponsive, mediaQuery, isFirst, prevMediaQuery
+          );
         }
       } else if (imgSize) {
 
       }
-      this.addStyles(cssQueries);
+      this.addStyles(cssQueries, nextIndex);
     } else {
-      this.addStyles(this.generateMediaQueries(elem, imgType, imgSize, imgParams, imgSrc, isResponsive));
+      this.addStyles(this.generateMediaQueries(elem, imgType, imgSize, imgParams, imgSrc, isResponsive), nextIndex);
     }
   };
 
-  jScaler.addStyles = function (css) {
-    var style = document.createElement('style');
+  jScaler.addStyles = function (css, nextIndex) {
+    var style = document.querySelector('.ci-img-index_' + nextIndex);
 
-    style.type = 'text/css';
-    if (style.styleSheet) {
-      style.styleSheet.cssText = css;
-    } else {
-      style.appendChild(document.createTextNode(css));
+    if (!style) {
+      style = document.createElement('style');
+      style.classList.add('ci-img-index_' + nextIndex);
+      style.type = 'text/css';
+
+      if (style.styleSheet) {
+        style.styleSheet.cssText = css;
+      } else {
+        style.appendChild(document.createTextNode(css));
+      }
+      this.head.appendChild(style);
     }
-    this.head.appendChild(style);
+
+    style.innerHTML = css;
   };
 
   jScaler.isLocalURL = function (elem, sourceUrl) {
@@ -303,19 +424,44 @@
 
   jScaler.init();
 
-  function isImgSizeIsObject(imgSize) {
-    return imgSize.length > 2 && imgSize.indexOf('{') === 0 && imgSize.indexOf('}') === imgSize.length - 1; // TODO:
-                                                                                                            // rectify
-  }
-
   function onerrorImg(event) {
-    this.removeEventListener('error', onerrorImg, false);
-    this.addEventListener('error', onerrorImgStep2, false);
-    this.src = this.getAttribute('ci-local-url');
+    // set default picture
+    //this.src = 'https://placeimg.com/500/500/animals';
   }
 
-  function onerrorImgStep2() {
-    this.removeEventListener('error', onerrorImgStep2, false);
-    this.src = 'https://placeimg.com/500/500/animals'; //TODO: ask for default picture
+  function getParentContainerWithWidth(img) {
+    var parentNode = null;
+    var width = 0;
+
+    do {
+      parentNode = (parentNode && parentNode.parentNode) || img.parentNode;
+      width = parentNode.getBoundingClientRect().width;
+    } while (parentNode && !width)
+
+    return width;
   }
+
+  function filterImages(images, type) {
+    var filtered = [];
+
+    for (var j = 0; j < images.length; j++) {
+      var image = images[j];
+      var size = image.getAttribute(type) || '';
+
+      if (size.slice(-4).toLowerCase() !== '.svg')
+        filtered.push(image);
+    }
+
+    return filtered;
+  }
+
+  var resizeTimer;
+
+  window.addEventListener('resize', function () {
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(function () {
+      jScaler.process(jScaler.config);
+    }, 350);
+  });
+
 })(window, document);
