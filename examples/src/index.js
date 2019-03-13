b95934a48047caa8168078913346fd745b1336ef
dmitry <dmitry@scaleflex.cloud>
2019-03-13 20:06:34 +0200

    add highlight for code sections

--- examples/src/index.js
+++ examples/src/index.js
@@ -1,6 +1,11 @@
 import '../../src';
 import './style.css';
 import './assets/fonts/helvetica-neue.css';
+import hljs from 'highlight.js/lib/highlight';
+import javascript from 'highlight.js/lib/languages/javascript';
+import 'highlight.js/styles/github.css';
+hljs.registerLanguage('javascript', javascript);
+hljs.initHighlightingOnLoad();
 
 const spinner = document.getElementById('spinner');
 const wrapper = document.getElementById('main');
