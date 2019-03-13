b480447d03e4097e30d0eb791ea65945da380472
dmitry <dmitry@scaleflex.cloud>
2019-03-13 14:14:29 +0200

    update demo page

--- examples/src/index.js
+++ examples/src/index.js
@@ -1,11 +1,14 @@
 import '../../src';
 import './style.css';
+import './assets/fonts/helvetica-neue.css';
 
+const spinner = document.getElementById('spinner');
+const wrapper = document.getElementById('main');
 const ciResponsive = new window.CIResponsive({
   token: 'demo',
   baseUrl: 'https://cloudimage.public.airstore.io/demo/',
   filters: 'q35.foil1',
-  queryString: '?&size_info=1',
+  queryString: '?&size_info=1&v2',
   lazyLoadOffset: 100,
   lazyLoading: true
 });
@@ -14,3 +17,6 @@ const ciResponsive = new window.CIResponsive({
 ciResponsive.init();
 
 window.lazySizes.init();
+
+wrapper.classList.add('active');
+spinner.style.display = 'none';
