9ed0405c30f7568b1e68b4560a536f34981b4c07
dmitry <dmitry@scaleflex.cloud>
2019-03-16 12:40:15 +0200

    display image's container width and users's device pixel ratio

--- examples/src/index.js
+++ examples/src/index.js
@@ -4,6 +4,7 @@ import './assets/fonts/helvetica-neue.css';
 import hljs from 'highlight.js/lib/highlight';
 import javascript from 'highlight.js/lib/languages/javascript';
 import 'highlight.js/styles/github.css';
+import { debounce } from 'throttle-debounce';
 hljs.registerLanguage('javascript', javascript);
 hljs.initHighlightingOnLoad();
 
@@ -18,10 +19,26 @@ const ciResponsive = new window.CIResponsive({
   lazyLoading: true
 });
 
-
 ciResponsive.init();
 
 window.lazySizes.init();
 
 wrapper.classList.add('active');
 spinner.style.display = 'none';
+
+/*
+*  logic for image containers;
+*/
+
+const containerBox = document.querySelectorAll('.container-width-box');
+const devicePixelRatio = document.querySelector('#device-pixel-ratio span');
+const setBoxSizes = () => {
+  containerBox.forEach(box => {
+    box.querySelector('span').innerText = box.offsetWidth;
+  });
+}
+
+setBoxSizes();
+
+devicePixelRatio.innerText = Math.round(window.devicePixelRatio || 1);
+window.onresize = debounce(400, setBoxSizes);
