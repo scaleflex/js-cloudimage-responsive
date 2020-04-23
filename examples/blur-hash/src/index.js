095ff6f094f7feccd600c0012034d204bbebf569
dmitry <dmitry@scaleflex.cloud>
2020-04-23 20:09:06 +0200

    refactor: naming

--- examples/blur-hash/src/index.js
+++ examples/blur-hash/src/index.js
@@ -13,7 +13,7 @@ const wrapper = document.getElementById('main');
 
 window.ciResponsive = new window.CIResponsive({
   token: 'demo',
-  baseUrl: 'https://cloudimage.public.airstore.io/demo/',
+  baseURL: 'https://cloudimage.public.airstore.io/demo/',
   params: 'ci_info=1&org_if_sml=1&version=16.04.2020',
   lazyLoadOffset: 100,
   lazyLoading: true,
