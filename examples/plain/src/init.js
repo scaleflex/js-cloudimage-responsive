722d1ce160fe24215e614a49bb045f6d09a3a867
Amr Wagdy <wagdy@scaleflex.com>
2022-03-19 18:39:46 +0200

    init: plain-version demo

--- examples/plain/src/init.js
+++ examples/plain/src/init.js
@@ -7,56 +7,6 @@ window.ciResponsive = new window.CIResponsive({
   lazyLoading: true,
   exactSize: false,
   limitFactor: 10,
-
-  // processURL: (props) => {
-  //   console.log(props)
-  //
-  //   return props.url;
-  // },
-  // props: {
-  //   query,
-  //   widthQ,
-  //   heightQ,
-  //   restParamsQ,
-  //   processOnlyWidth,
-  //   devicePixelRatio,
-  //   service: {
-  //     methods: {},
-  //     props: {
-  //       imgNode,
-  //       imgProps,
-  //       config
-  //     }
-  //   }
-  // }
-  // processQueryString: (props) => {
-  //   var imgNode = props.service.props.imgNode;
-  //   var maxWidth = imgNode.getAttribute('max-width');
-  //   var maxHeight = imgNode.getAttribute('max-height');
-  //
-  //   if (maxWidth && maxHeight) {
-  //     return [
-  //       `${props.restParamsQ}`,
-  //       `&w=${maxWidth * props.devicePixelRatio}&h=${maxHeight * props.devicePixelRatio}&func=fit`
-  //     ].join('');
-  //   } else {
-  //     return props.query;
-  //   }
-  // }
-
-  // imgSelector: 'data-src',
-  // bgSelector: 'data-bg-src'
-
-  //ignoreNodeImgSize: false,
-  //saveNodeImgRatio: false,
-  //ignoreStyleImgSize: false,
-  //destroyNodeImgSize: false,
-  //detectImageNodeCSS: false,
-  //processOnlyWidth: false,
-  //
-  //onImageLoad: function (image) {
-  //  console.log(image.width, image.height);
-  //}
 });
 
 window.lazySizes.init();
